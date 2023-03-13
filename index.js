e534108c06d9f23dd7e0c7a02112820e507da81c
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-03-13 20:50:32 +0100

    Add toggle mobile menu

---
+++ index.js
@@ -0,0 +1,22 @@
+const bars = document.getElementById('bars');
+const menu = document.getElementById('menu');
+
+const closeMenu = () => {
+    let barsChild = bars.children[0];
+    if (barsChild.classList.contains('fa-bars')) {
+        barsChild.classList.remove('fa-bars');
+        barsChild.classList.add('fa-times');
+        menu.classList.add('top-[80px]');
+        menu.classList.add('opacity-100');
+    }
+    else {
+        barsChild.classList.remove('fa-times');
+        barsChild.classList.add('fa-bars');
+        menu.classList.remove('opacity-100');
+        menu.classList.remove('top-[80px]');
+    }
+}
+
+
+
+bars.addEventListener('click', closeMenu);
